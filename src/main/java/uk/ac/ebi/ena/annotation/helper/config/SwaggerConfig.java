2b276307bbfb2a4451a65b3cdefbe92c59794281
Vikas Gupta <vikasg@ebi.ac.uk>
2022-06-24 12:17:26 +0100

    swagger mapping changed

--- src/main/java/uk/ac/ebi/ena/annotation/helper/config/SwaggerConfig.java
+++ src/main/java/uk/ac/ebi/ena/annotation/helper/config/SwaggerConfig.java
@@ -21,20 +21,25 @@ package uk.ac.ebi.ena.annotation.helper.config;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
 import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
 @EnableSwagger2
 @ImportAutoConfiguration({SecurityConfiguration.class})
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 
     /**
      * Swagger API Details
@@ -52,6 +57,18 @@ public class SwaggerConfig {
     }
 
     /**
+     * set the doc-list expansion.
+     *
+     * @return
+     */
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .docExpansion(DocExpansion.LIST) // or DocExpansion.NONE or DocExpansion.FULL
+                .build();
+    }
+
+    /**
      * Method to build the api information
      *
      * @return ApiInfo
@@ -61,6 +78,22 @@ public class SwaggerConfig {
                 .title("ENA Source Annotations Helper (SAH)")
                 .version("1.0.0")
                 .build();
+    }
+
+    /**
+     * Set the forwards for swagger-ui handling.
+     *
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // forward requests to /ena/sah/api/ to the index.html and swagger mappings
+        registry.addViewController("/ena/sah/api/").setViewName(
+                "forward:/ena/sah/api/index.html");
+        registry.addViewController("/ena/sah/api").setViewName(
+                "forward:/ena/sah/api/index.html");
+        registry.addViewController("/ena/sah/api/v2/api-docs").setViewName(
+                "forward:/v2/api-docs");
     }
 
 }
