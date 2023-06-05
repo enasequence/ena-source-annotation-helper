c02a31c75a1f343243cafd380b662df569e63fb1
Vikas Gupta <vikasg@ebi.ac.uk>
2023-06-05 09:58:33 +0100

    fixed issue for single letter code institution

--- biocollections-importer/src/main/java/uk/ac/ebi/ena/sah/biocollections/importer/entity/Institution.java
+++ biocollections-importer/src/main/java/uk/ac/ebi/ena/sah/biocollections/importer/entity/Institution.java
@@ -48,7 +48,7 @@ public class Institution {
     @JsonProperty("coll_type")
     private String collType;
     @JsonProperty("qualifier_type")
-    private String qualifierType;
+    private String[] qualifierType;
 
     @JsonProperty("home_url")
     private String homeUrl;
@@ -72,4 +72,11 @@ public class Institution {
                 ", urlRule='" + urlRule + '\'' +
                 '}';
     }
+
+    public void addQualifierType(String qtype) {
+        if(qtype != null) {
+            qualifierType = qtype.split(",");
+        }
+    }
+
 }
